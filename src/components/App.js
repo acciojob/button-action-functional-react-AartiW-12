44d5faff824bd4c45bb28b23d79238ab5fefabb2
AartiW-12 <aartiwhaval12@gmail.com>
2025-12-05 12:32:40 +0530

    Added functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [visible, setVisible] = useState(false)
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id ="click" onClick = {()=> setVisible(!visible)}>Show Para</button>
+        {visible && 
+           <p id="para" >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        }
     </div>
   );
 }
-
 
 export default App;
